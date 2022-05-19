4c5933fdf5673a238d04ad889df7fee8d7f4070b
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-05-19 18:53:25 +0900

    ✨ GET Method with ID

--- src/components/DrfApiFetch.jsx
+++ src/components/DrfApiFetch.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 export const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState([]);
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
@@ -16,6 +18,18 @@ export const DrfApiFetch = () => {
       });
   }, []);
 
+  const getTask = () => {
+    axios
+      .get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
+        headers: {
+          Authorization: "Token 80354b461d950032f7bb80feadd24b8d7869645f",
+        },
+      })
+      .then((res) => {
+        setSelectedTask(res.data);
+      });
+  };
+
   return (
     <div>
       <ul>
@@ -25,6 +39,22 @@ export const DrfApiFetch = () => {
           </li>
         ))}
       </ul>
+      Set id <br />
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => {
+          setId(evt.target.value);
+        }}
+      />
+      <br />
+      <button type="button" onClick={() => getTask()}>
+        Get task
+      </button>
+      <h3>
+        {selectedTask.title}
+        {selectedTask.id}
+      </h3>
     </div>
   );
 };
